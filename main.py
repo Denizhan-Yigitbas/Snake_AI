370463450a3e24a82cb577002b8805acd0b8c345
Denizhan Yigitbas <denizhanyigitbas@gmail.com>
2018-12-12 23:42:13 -0700

    commenting

--- main.py
+++ main.py
@@ -19,8 +19,9 @@ class App:
         self._running = True
         self._display_surf = None
         self.size = self.weight, self.height = 400, 400
+        
+        #Initial Snake array with 3 Snake Blocks starting at (50, 50) and going left
         self.snake = [Snake(WHITE, 10, 10, 50, 50), Snake(WHITE, 10, 10, 40, 50), Snake(WHITE, 10, 10, 30, 50)]
-
         
     
     def on_init(self):
@@ -28,46 +29,75 @@ class App:
         self._display_surf = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
         self._display_surf.fill(BLACK)
         self._running = True
-        xInit = 100
-        yInit = 100
+        
+        # display the initial Snake array
         for i in range(len(self.snake)):
             self._display_surf.blit(self.snake[i].image, self.snake[i].rect)
-            # player = self.snake[i].image
-            # position = player.get_rect()
-            # position.x = xInit
-            # position.y = yInit
-            # self._display_surf.blit(self.snake[i].image, position)
-            # xInit -= 10
-            # self.snake.append(Snake(WHITE, 10, 10, position.x, position.y))
         pygame.display.update()
         
-        # for i in range(len(self.snake)):
-        #     player = self.snake[i].image
-        #     position = player.get_rect()
-        #     for x in range(100):
-        #         self._display_surf.fill(BLACK)
-        #         position = position.move(10,0)
-        #         self._display_surf.blit(player, position)
-        #         pygame.display.update()
 
 
     """
     Helper Method that will run the events that are clicked on by the user
     """
     def on_event(self):
-        # movement = {pygame.K_UP: (0, -1),
-        #             pygame.K_DOWN: (0, 1),
-        #             pygame.K_LEFT: (-1, 0),
-        #             pygame.K_RIGHT: (1, 0)}
-        move = (0,0)
         
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self._running = False
             if event.type == pygame.KEYDOWN:
+                
+                # Adds a block to the end of the snake. This will replace when snake eats a Food
+                if event.key == pygame.K_SPACE:
+                    print("SPACE")
+                    
+                    # Erases the current screen
+                    self._display_surf = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
+                    self._display_surf.fill(BLACK)
+                    
+                    # Store the last and second to last blocks of the snake
+                    lastSnakeBlock = self.snake[-1]
+                    secondToLastBlock = self.snake[-2]
+                    
+                    # if the last two blocks are on the same horizontal line and the last block is to the left of the
+                    # second to last block, add a block to the left side of the last block
+                    if lastSnakeBlock.rect.y == secondToLastBlock.rect.y and lastSnakeBlock.rect.x < secondToLastBlock.rect.x:
+                        newX = lastSnakeBlock.rect.x - 10
+                        newSnakeBlock = Snake(lastSnakeBlock.color, lastSnakeBlock.width, lastSnakeBlock.height, newX,
+                                              lastSnakeBlock.rect.y)
+                        self.snake.append(newSnakeBlock)
+
+                    # if the last two blocks are on the same horizontal line and the last block is to the right of the
+                    # second to last block, add a block to the right side of the last block
+                    if lastSnakeBlock.rect.y == secondToLastBlock.rect.y and lastSnakeBlock.rect.x > secondToLastBlock.rect.x:
+                        newX = lastSnakeBlock.rect.x + 10
+                        newSnakeBlock = Snake(lastSnakeBlock.color, lastSnakeBlock.width, lastSnakeBlock.height, newX,
+                                              lastSnakeBlock.rect.y)
+                        self.snake.append(newSnakeBlock)
+                        
+                    # if the last two blocks are on the same vertical line and the last block is above the
+                    # second to last block, add a block above the last block
+                    if lastSnakeBlock.rect.x == secondToLastBlock.rect.x and lastSnakeBlock.rect.y < secondToLastBlock.rect.y:
+                        newY = lastSnakeBlock.rect.y - 10
+                        newSnakeBlock = Snake(lastSnakeBlock.color, lastSnakeBlock.width, lastSnakeBlock.height,
+                                              lastSnakeBlock.rect.x, newY)
+                        self.snake.append(newSnakeBlock)
+
+                    # if the last two blocks are on the same vertical line and the last block is below the
+                    # second to last block, add a block below the last block
+                    if lastSnakeBlock.rect.x == secondToLastBlock.rect.x and lastSnakeBlock.rect.y > secondToLastBlock.rect.y:
+                        newY = lastSnakeBlock.rect.y + 10
+                        newSnakeBlock = Snake(lastSnakeBlock.color, lastSnakeBlock.width, lastSnakeBlock.height,
+                                              lastSnakeBlock.rect.x, newY)
+                        self.snake.append(newSnakeBlock)
+                        
+
+                    # displays all snake Blocks
+                    print(len(self.snake) )
+                    for i in range(len(self.snake)):
+                        self._display_surf.blit(self.snake[i].image, self.snake[i].rect)
+                    
                 if event.key == pygame.K_LEFT:
-                    # self.body.pop()
-                    # self.body.append(self.snake.moveLeft())
                     print("LEFT")
 
                     # Erases the current screen
@@ -96,7 +126,6 @@ class App:
                         self._display_surf.blit(self.snake[i].image, self.snake[i].rect)
                         
                 if event.key == pygame.K_RIGHT:
-                    # TODO: make Snake move to the right
                     print("RIGHT")
                     
                     # Erases the current screen
@@ -126,8 +155,6 @@ class App:
             
             
                 if event.key == pygame.K_UP:
-                    # TODO: make Snake move up
-                    
                     # Erases the current screen
                     self._display_surf = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
                     self._display_surf.fill(BLACK)
@@ -154,7 +181,6 @@ class App:
                         self._display_surf.blit(self.snake[i].image, self.snake[i].rect)
                         
                 if event.key == pygame.K_DOWN:
-                    # TODO: make Snake move down
                     # Erases the current screen
                     self._display_surf = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
                     self._display_surf.fill(BLACK)
@@ -181,14 +207,6 @@ class App:
                     # displays the shifted snake Blocks
                     for i in range(len(self.snake)):
                         self._display_surf.blit(self.snake[i].image, self.snake[i].rect)
-        # self.snake.rect.move_ip(move)
-        # self._display_surf.fill(pygame.color.Color('Black'))
-        # pygame.draw.rect(self._display_surf, pygame.color.Color('White'), self.snake.rect)
-        # pygame.display.update()
-        # pygame.draw.rect(self._display_surf, WHITE, self.snake.rect)
-            # all_sprite_list.empty()
-            # all_sprite_list.add(self.body)
-            # all_sprite_list.draw(self._display_surf)
         pygame.display.update()
                     
     def on_loop(self):
@@ -199,6 +217,7 @@ class App:
 
     def on_cleanup(self):
         pygame.quit()
+            
 
     """
     Game Loop
@@ -212,6 +231,8 @@ class App:
             self.on_loop()
             self.on_render()
         self.on_cleanup()
+        
+    
 
 
 if __name__ == "__main__":
